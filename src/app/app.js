8b68b53464c221423d38dc8d5d69e9e950d0eb04
Eric Tonussi <erictonussi@gmail.com>
2017-05-04 22:33:35 -0300

    inicio da criação do popup

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('app')
-  .controller('AppController', function ($rootScope, user, teamPreview, $window) {
+  .controller('AppController', function ($rootScope, $scope, $window, ngDialog, user, teamPreview, teamMatchesAlert) {
 
     var vm = this;
 
@@ -8,5 +8,26 @@ angular.module('app')
     vm.teamPreview = teamPreview.data.data;
 
     $rootScope.$windowUser = $window.user;
+
+    if ( teamMatchesAlert.data.data.Matches.length ) {
+
+      vm.teamMatchesAlert = teamMatchesAlert.data.data.Matches;
+
+      ngDialog.open({
+        template: 'matches-alert.html',
+        appendClassName: 'ngdialog-matches-alert',
+        // controller: 'MainConfigController as $ctrl',
+        scope: $scope
+        // resolve: {
+        //   settings: function () {
+        //     return MainService.teamSettings()
+        //       .then(function (result) {
+        //         return result.data.data;
+        //       });
+        //   }
+        // },
+      });
+
+    }
 
   });
--- src/routes.js
+++ src/routes.js
@@ -25,6 +25,9 @@ function routesConfig ($stateProvider, $urlRouterProvider/*, $locationProvider*/
         teamPreview: function (AppService) {
           return AppService.teamPreview();
         },
+        teamMatchesAlert: function (AppService) {
+          return AppService.teamMatchesAlertView();
+        }
       },
       // component: 'app'
       templateUrl: 'app.html',
