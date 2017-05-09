be310f2f090c5a9e97e149e2bc0658cb752d4299
Eric Tonussi <erictonussi@gmail.com>
2017-05-09 00:30:16 -0300

    ajustes de proxy na tela de itens

--- src/app/01-itens/itens.js
+++ src/app/01-itens/itens.js
@@ -6,8 +6,8 @@ function itensController (ItensService, inventory, storeList) {
 
   var vm = this;
 
-  vm.inventory = inventory.data.data;
-  vm.storeList = storeList.data.data;
+  vm.inventory = inventory;
+  vm.storeList = storeList;
 
   vm.saleItens = []
     .concat( vm.storeList.TeamPlayerItems || [] )
@@ -64,21 +64,33 @@ function itensRoutesConfig ($stateProvider) {
           var data, itens;
 
           if ( $stateParams.inventario === 'inventario' ) {
-            data = inventory.data.data;
+            data = inventory;
           } else {
-            data = storeList.data.data;
+            data = storeList;
           }
 
           if ( $stateParams.centro === 'centro' ) {
+            // if (_storeTCItems[i].Category == 1 || _storeTCItems[i].Category == 3)
+            //   _storeTcDItems.push(_storeTCItems[i]);
+            // if (_storeTCItems[i].Category == 4 || _storeTCItems[i].Category == 5)
+            //   _storeTcCItems.push(_storeTCItems[i]);
+            // if (_storeTCItems[i].Category == 6 || _storeTCItems[i].Category == 7)
+            //   _storeTcBItems.push(_storeTCItems[i]);
+            // if (_storeTCItems[i].Category == 8 || _storeTCItems[i].Category == 9)
+
             itens = data.TrainingCenterItems.filter(function (item) {
-              return item.League === $stateParams.category;
+              return {
+                D: [1, 3],
+                C: [4, 5],
+                B: [6, 7],
+                A: [8, 9]
+              }[$stateParams.category].indexOf(item.Category) !== -1;
             });
           } else {
             itens = data.TeamPlayerItems.filter(function (item) {
               return item.Category === parseInt($stateParams.category);
             });
           }
-
           return itens;
 
         }
--- src/app/01-itens/itens.serv.js
+++ src/app/01-itens/itens.serv.js
@@ -1,15 +1,15 @@
 'use strict';
 angular.module('app')
-  .service('ItensService', function ($http) {
+  .service('ItensService', function (PostToJs) {
 
     var vm = this;
 
     vm.inventory = function () {
-      return $http.get('/data/Team/Inventory');
+      return PostToJs('Team/Inventory');
     };
 
     vm.storeList = function () {
-      return $http.get('/data/Store/List');
+      return PostToJs('Store/List');
     };
 
   });
