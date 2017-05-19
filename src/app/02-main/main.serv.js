58bc4fa220b87fa928581c44a91ac57cc0fa1e92
Eric Tonussi <erictonussi@gmail.com>
2017-05-19 12:42:23 -0300

    merge das alterações do Giuliano

--- src/app/02-main/main.serv.js
+++ src/app/02-main/main.serv.js
@@ -1,19 +1,19 @@
 'use strict';
 angular.module('app')
-  .service('MainService', function (PostToJs, $window) {
+  .service('MainService', function ($http) {
 
     var vm = this;
 
     vm.teamSettings = function () {
-      return PostToJs('Team/Settings');
+      return $http.get('/data/Team/Settings');
     };
 
     vm.teamTrophyRoom = function () {
-      return PostToJs('Team/TrophyRoom', {snId: $window.idSocialNetwork});
+      return $http.get('/data/Team/TrophyRoom');
     };
 
     vm.teamStats = function () {
-      return PostToJs('Team/Statistics');
+      return $http.get('/data/Team/Statistics');
     };
 
   });
