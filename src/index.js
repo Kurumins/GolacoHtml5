f247aa4daee21842a17998c72652b12936448a3e
Eric Tonussi <erictonussi@gmail.com>
2017-05-21 13:15:52 -0300

    configurando o loader

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ angular
     'ngDialog',
     'lvl.directives.dragdrop',
     'nsPopover',
-    'angular.filter'
+    'angular.filter',
+    'darthwade.dwLoading'
   ])
   .config(function ($qProvider) {
     $qProvider.errorOnUnhandledRejections(false);
   })
+
   .run(function ($window) {
 
     $window._gaq = [];
@@ -22,7 +24,7 @@ angular
 
   })
 
-  .factory('PostToJs', function ($window, $q) {
+  .factory('PostToJs', function ($window, $q, $loading) {
 
     var vm = this;
 
@@ -38,12 +40,23 @@ angular
       var deferred = $q.defer();
 
       vm.callbacks[action] = function (result) {
+
         if ( result.Success !== false) {
           deferred.resolve(result);
         } else {
           deferred.reject(result);
         }
+
+        // debugger;
+        delete vm.callbacks[action];
+
+        if ( Object.size(vm.callbacks) === 0 ) {
+          $loading.finish('PostToJs');
+        }
+
       };
+
+      $loading.start('PostToJs');
 
       $window.doAction(action, action, data, signed || true, cacheId || +new Date() );
 
@@ -51,3 +64,11 @@ angular
 
     };
   });
+
+Object.size = function(obj) {
+  var size = 0, key;
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++;
+  }
+  return size;
+};
