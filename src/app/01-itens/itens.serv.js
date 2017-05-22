bef428279d1caaec2fb6f472e9a1e2b0c36c4fc8
Eric Tonussi <erictonussi@gmail.com>
2017-05-22 14:20:14 -0300

    compra de itens

--- src/app/01-itens/itens.serv.js
+++ src/app/01-itens/itens.serv.js
@@ -12,4 +12,17 @@ angular.module('app')
       return PostToJs('Store/List');
     };
 
+    // data[BundleId]:7
+    // data[TypeId]:1
+    // data[ItemQuantity]:1
+    // data[ItemId]:187
+    vm.itemPurchase = function (item, qty) {
+      return PostToJs('Store/Purchase', {
+        TypeId: [1, 3, 4, 5, 6, 7, 8, 9].indexOf(item.Category) === -1 ? 1 : 2,
+        ItemQuantity: qty,
+        ItemId: item.Id,
+        BundleId: item.IdBundle
+      });
+    };
+
   });
