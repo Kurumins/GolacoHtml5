bbd9a605a45af977b0194886f8598b324cbefa52
Eric Tonussi <erictonussi@gmail.com>
2017-05-30 12:11:20 -0300

    finalização do layout da tabela

--- src/app/10-calendar/calendar.js
+++ src/app/10-calendar/calendar.js
@@ -5,6 +5,18 @@ angular.module('app')
 function calendarController (matchCalendar) {
   var vm = this;
 
+  vm.traversal = {
+    1: 'league',
+    2: 'tournament',
+    3: 'cup',
+    4: 'mini tournament',
+    5: 'mini cup',
+    6: 'copa nordeste',
+    7: 'tournament beta',
+    100: 'friendly',
+    101: 'friendly',
+  }
+
   vm.matchCalendar = matchCalendar;
   vm.matches = matchCalendar.Matches;
 }
