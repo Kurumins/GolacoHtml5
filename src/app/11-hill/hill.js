b14c8a4ece34d900948388e72b4a271f13334b4f
Eric Tonussi <erictonussi@gmail.com>
2017-06-07 23:11:39 -0300

    finalização do layout da tela Rei da Mesa

--- src/app/11-hill/hill.js
+++ src/app/11-hill/hill.js
@@ -2,10 +2,20 @@
 angular.module('app')
   .config(hillRoutesConfig);
 
-function hillController (preview) {
+function hillController (preview, HillService, $timeout) {
   var vm = this;
 
   vm.preview = preview;
+
+  vm.refresh = function () {
+    HillService.preview()
+      .then(function (preview) {
+        delete vm.preview;
+        $timeout( function () {
+          vm.preview = preview;
+        })
+      });
+  };
 
 }
 
