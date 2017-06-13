bab1d8e4b00ed19cf12ca3e6b87f6daf270715c0
Eric Tonussi <erictonussi@gmail.com>
2017-06-13 16:42:36 -0300

    trantando erros de html para não quebrar o Gulp

--- gulp_tasks/partials.js
+++ gulp_tasks/partials.js
@@ -10,10 +10,12 @@ gulp.task('partials', partials);
 function partials() {
   return gulp.src(conf.path.src('app/**/*.html'))
     .pipe(htmlmin(conf.htmlmin))
+    .on('error', conf.errorHandler('htmlmin'))
     .pipe(rename({dirname: '/'}))
     .pipe(angularTemplatecache('templateCacheHtml.js', {
       module: conf.ngModule,
       root: ''
     }))
+    .on('error', conf.errorHandler('angularTemplatecache'))
     .pipe(gulp.dest(conf.path.tmp()));
 }
